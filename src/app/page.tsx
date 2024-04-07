207801864adeee25696fbc47659d0643d1885e42
jia-wei-00 <wwwbingo6@gmail.com>
2024-04-08 01:19:18 +0800

    update card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 const Home = () => {
   const papers = [
     {
-      text: "Thank you for taking the time to read this. Not a scam promise 🤜🤛",
+      text: "Thank you for taking the time to read this. Not a scam, I promise 🤜🤛",
       rotation: getRandomRotation(-25, 25),
     },
     {
